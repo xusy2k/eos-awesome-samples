cc02fbb74a53dc64f8685c74105c8b5ebeebdf53
Xus <xus@zoyolabs.com>
2018-06-23 16:43:58 +0200

    Remove exit

--- contracts/init-chain.sh
+++ contracts/init-chain.sh
@@ -3,7 +3,6 @@
 INITIAL_PWD=`pwd`
 
 cd ${INITIAL_PWD}; cd ping; ./init-chain.sh "${@}"
-exit;
 cd ${INITIAL_PWD}; cd todo; ./init-chain.sh "${@}"
 cd ${INITIAL_PWD}; cd pet; ./init-chain.sh "${@}"
 
